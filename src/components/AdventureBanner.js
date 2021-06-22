146d7b72c3f9d3510d0e87003fb647733b9dcf79
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-06-22 17:21:52 +1000

    form added to adventure banner compoenents

--- src/components/AdventureBanner.js
+++ src/components/AdventureBanner.js
@@ -5,7 +5,64 @@ import Fade from "react-reveal/Fade";
 const AdventureBanner = () => {
   return (
     <AdventureSection>
-      <div className='banner'></div>
+      <div className="banner">
+        <form className="flex-form">
+          <h1 style={{color: 'white', fontWeight: '500'}}>Search</h1>
+          <div>
+            <select name="Make" id="" className="select">
+              <option value="0">Make:</option>
+              <option value="1">Audi</option>
+              <option value="2">BMW</option>
+              <option value="3">Citroen</option>
+              <option value="4">Ford</option>
+              <option value="5">Honda</option>
+              <option value="6">Jaguar</option>
+              <option value="7">Land Rover</option>
+              <option value="8">Mercedes</option>
+              <option value="9">Mini</option>
+              <option value="10">Nissan</option>
+              <option value="11">Toyota</option>
+              <option value="12">Volvo</option>
+            </select>
+          </div>
+          <div>
+            <select name="Model" id="" className="select">
+              <option value="0">Model:</option>
+              <option value="1">Audi</option>
+              <option value="2">BMW</option>
+              <option value="3">Citroen</option>
+              <option value="4">Ford</option>
+              <option value="5">Honda</option>
+              <option value="6">Jaguar</option>
+              <option value="7">Land Rover</option>
+              <option value="8">Mercedes</option>
+              <option value="9">Mini</option>
+              <option value="10">Nissan</option>
+              <option value="11">Toyota</option>
+              <option value="12">Volvo</option>
+            </select>
+          </div>
+          <div>
+            <select name="Location" id="" className="select">
+              <option value="0">Location:</option>
+              <option value="1">Melbourne</option>
+              <option value="2">Sydney</option>
+              <option value="3">Brisbane</option>
+              <option value="4">Darwin</option>
+              <option value="5">Perth</option>
+              <option value="6">Adelaide</option>
+              <option value="7">Canberra</option>
+            </select>
+          </div>
+          <div>
+            <input type="text" className="select" placeholder="Keywords" />
+          </div>
+
+          <div>
+            <input type="submit" value="Go" />
+          </div>
+        </form>
+      </div>
     </AdventureSection>
   );
 };
@@ -19,6 +76,36 @@ const AdventureSection = styled.section`
     background-size: cover;
     background-postion: center;
     background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .flex-form {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      justify-content: space-evenly;
+      height: 450px;
+      width: 450px;
+      background: rgba(81, 163, 189, 0.6);
+    }
+    .select {
+      height: 30px;
+      width: 300px;
+      border-radius: 3px;
+    }
+    .flex-form input[type="submit"] {
+      background: #48b9d3;
+      border: 1px solid #48b9d3;
+      color: #fff;
+      height: 30px;
+      width: 100px;
+      border-radius: 2px;
+      padding: 0 30px;
+      cursor: pointer;
+      -webkit-transition: all 0.2s;
+      -moz-transition: all 0.2s;
+      transition: all 0.2s;
+    }
   }
 `;
 export default AdventureBanner;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,10 +8,10 @@ const Landing = () => {
       <Fade bottom cascade>
         <div className="welcome-text">
           <h1>Find your next car</h1>
-          {/* <form className="flex-form">
-          <input type="search" placeholder="Dream Car?" />
-          <input type="submit" value="Go" />
-        </form> */}
+          <form className="flex-form">
+            <input type="search" placeholder="I'm looking for.." />
+            <input type="submit" value="Go" />
+          </form>
         </div>
       </Fade>
       <Fade top>
@@ -45,6 +45,50 @@ const LandingSection = styled.section`
       font-size: 3rem;
       font-weight: 500;
       text-shadow: #a59e98 0px 0px 5px;
+    }
+    .flex-form input[type="submit"] {
+      background: #48b9d3;
+      border: 0.5px solid #48b9d3;
+      color: #fff;
+      padding: 0 30px;
+      cursor: pointer;
+      -webkit-transition: all 0.2s;
+      -moz-transition: all 0.2s;
+      transition: all 0.2s;
+    }
+
+    .flex-form input[type="submit"]:hover {
+      background: #69cbe0;
+      border: 1px solid #69cbe0;
+    }
+
+    .flex-form {
+      display: -webkit-box;
+      display: flex;
+      z-index: 10;
+      position: relative;
+      width: 351px;
+      height: 40px;
+      margin-top: 20px;
+    }
+
+    .flex-form > * {
+      border: 0;
+      padding: 0 0 0 10px;
+      background: #fff;
+      font-size: 1rem;
+      border-radius: 0;
+      outline: 0;
+      -webkit-appearance: none;
+      opacity: 0.5;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+
+    input[type="search"] {
+      flex-basis: 500px;
     }
   }
   a {
