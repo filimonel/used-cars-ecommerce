34fa649464c3a783592cbbca51b316fc299677d3
Filimone Leaaetoa <filimone.leaaetoa@gmail.com>
2021-06-22 10:14:28 +1000

    added welcome text and search bar to banner

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,13 @@ import Fade from "react-reveal";
 const Landing = () => {
   return (
     <LandingSection>
-      <FormWrapper></FormWrapper>
+      <div className="welcome-text">
+        <h1>Find your next car</h1>
+        <form className="flex-form">
+          <input type="search" placeholder="Dream Car?" />
+          <input type="submit" value="Go" />
+        </form>
+      </div>
     </LandingSection>
   );
 };
@@ -20,8 +26,63 @@ const LandingSection = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
+  .welcome-text {
+    color: white;
+    height: 50%;
+    width: 50%;
+    display: flex;
+    align-items: center;
+    // justify-content: center;
+    flex-direction: column;
+
+    h1 {
+      font-size: 3.5rem;
+      font-weight: 500;
+      text-shadow: #c9b4ae 0px 0px 5px;
+    }
+  }
+  .flex-form input[type="submit"] {
+    background: #234546;
+    border: 1px solid #234546;
+    color: #fff;
+    padding: 0 30px;
+    cursor: pointer;
+    -webkit-transition: all 0.2s;
+    -moz-transition: all 0.2s;
+    transition: all 0.2s;
+  }
+  .flex-form input[type="submit"]:hover {
+    background: #69cbe0;
+    border: 1px solid #69cbe0;
+  }
+  .flex-form {
+    display: -webkit-box;
+    display: flex;
+    z-index: 10;
+    position: relative;
+    width: 351px;
+    height: 40px;
+    margin-top: 30px;
+    opacity: 0.8;
+    transition: 1s ease;
+    &:hover {
+      opacity: 1;
+    }
+  }
+  .flex-form > * {
+    border: 0;
+    padding: 0 0 0 10px;
+    background: #fff;
+    font-size: 1rem;
+    border-radius: 0;
+    outline: 0;
+    -webkit-appearance: none;
+  }
+  input[type="search"] {
+    flex-basis: 500px;
+  }
 `;
 
-const FormWrapper = styled.div``;
+const LandingTextWrapper = styled.div``;
 
 export default Landing;
